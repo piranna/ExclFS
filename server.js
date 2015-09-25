ac5d431e1d7146521cdd1b2598ff5a28da069226
Jesús Leganés Combarro "piranna <piranna@gmail.com>
2015-09-26 00:58:06 +0200

    Accept mount-ish arguments
    See http://stackoverflow.com/a/1555146/586382

--- server.js
+++ server.js
@@ -7,21 +7,68 @@ var fuse = require('fuse-bindings')
 var ExclFS = require('./')
 
 
-if(process.argv.length < 4)
+// http://stackoverflow.com/a/1555146/586382
+function setOption(arg)
 {
-  console.error('Usage:', process.argv[1], '<mountPoint>', '<lowerLayer>',
-                '[whitelist]')
+  arg = arg.split('=')
+
+  var key, value
+
+  if(arg.length > 1)
+  {
+    key   = arg.shift()
+    value = arg.join('=')
+  }
+  else
+  {
+    key = arg
+    value = true
+  }
+
+  this[key] = value
+}
+
+function processOptions(arg)
+{
+  if(arg === '-o') continue
+
+  arg.split(',').forEach(setOption, this)
+}
+
+function parse(argv)
+{
+  var options = {}
+
+  var result =
+  {
+    dev:     argv[0],
+    path:    argv[1],
+    options: options
+  }
+
+  argv.slice(2).forEach(processOptions, options)
+
+  return result
+}
+
+
+var argv = process.argv
+if(argv.length < 4)
+{
+  console.error('Usage:', process.argv[1], '<dev>', '<path>',
+                '[-o whitelist=<whitelist>]')
   process.exit(1)
 }
 
-var mountPoint = process.argv[2]
-var lowerLayer = process.argv[3]
-var whitelist  = process.argv[4]
+var args = parse(argv.slice(2))
+
+var mountPoint = args.path
+var whitelist  = args.options.whitelist
 
 if(whitelist) whitelist = readFileSync(whitelist, 'utf8').split('\n')
 
 
-fuse.mount(mountPoint, ExclFS(lowerLayer, whitelist), function(error)
+fuse.mount(mountPoint, ExclFS(args.dev, whitelist), function(error)
 {
   if(error) console.error('ExclFS failed to mount:',error)
 })
