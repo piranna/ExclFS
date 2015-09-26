eab01e06993b964118a92d1b52ab096b2530e71c
Jesús Leganés Combarro "piranna <piranna@gmail.com>
2015-09-27 01:29:08 +0200

    Enable device files on the filesystem

--- index.js
+++ index.js
@@ -156,7 +156,7 @@ function ExclFS(lowerLayer, whitelist)
   }
 
   // [ToDO] Move to server.js when we are ready for nan2
-  this.options = ['nonempty']
+  this.options = ['dev', 'nonempty']
 }
 
 
