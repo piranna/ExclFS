e1d7bc9c4f65aa78eca51d6b24af74e8388d64b0
Jesús Leganés Combarro "piranna <piranna@gmail.com>
2015-09-26 16:21:27 +0200

    Set lowerLayer as option

--- server.js
+++ server.js
@@ -9,23 +9,28 @@ var ExclFS = require('./')
 var parse = require('./parseMountArgv')
 
 
-var argv = process.argv
-if(argv.length < 4)
+function usage()
 {
-  console.error('Usage:', process.argv[1], '<dev>', '<path>',
-                '[-o whitelist=<whitelist>]')
+  console.error('Usage:', process.argv[1], 'null', '<path>',
+                '-o lowerLayer=<lowerLayer>', '[-o whitelist=<whitelist>]')
   process.exit(1)
 }
+
+
+var argv = process.argv
+if(argv.length < 4) usage()
 
 var args = parse(argv.slice(2))
 
 var mountPoint = args.path
+var lowerLayer = args.options.lowerLayer
 var whitelist  = args.options.whitelist
 
+if(!lowerLayer) usage()
 if(whitelist) whitelist = readFileSync(whitelist, 'utf8').split('\n')
 
 
-fuse.mount(mountPoint, ExclFS(args.dev, whitelist), function(error)
+fuse.mount(mountPoint, ExclFS(lowerLayer, whitelist), function(error)
 {
   if(error) console.error('ExclFS failed to mount:',error)
 })
